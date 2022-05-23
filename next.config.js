68ce34e89a6c18854b8f2ed81ab12f442bc913f2
Jahidul Haque Rassel <jahidul.rassel@Jahiduls-MacBook-Pro.local>
2022-05-24 05:16:12 +0600

    update next.config

--- next.config.js
+++ next.config.js
@@ -11,10 +11,10 @@ module.exports = nextConfig
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: [process.env.NEXT_PUBLIC_DRUPAL_BASE_URL],
+    domains: [process.env.NEXT_IMAGE_DOMAIN],
   },
   env: {
-    backendUrl: process.env.NEXT_IMAGE_DOMAIN,
+    backendUrl: process.env.NEXT_PUBLIC_DRUPAL_BASE_URL,
     // set imageUrl if IMAGE_DOMAIN is set in env vars to override default
     imageUrl: `https://${process.env.NEXT_IMAGE_DOMAIN}`,
   },
