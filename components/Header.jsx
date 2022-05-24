b59ad5358b76e83bee1701731f1484026c240a28
Jahidul Haque Rassel <jahidul.rassel@Jahiduls-MacBook-Pro.local>
2022-05-25 02:30:46 +0600

    navBarFix

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-
+import Navbar from './Navbar'
 
 export default function Header() {
   return (
     <Navbar></Navbar>
   )
 }
+
+
